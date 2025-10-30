d064e4bb4ab92a051c28b485729f09c3047dcdaf
nigelwrightington <nigelwrightington@yahoo.com>
2025-10-29 23:41:56 -0400

    submitted final shutdown

---
+++ submission.sql
@@ -0,0 +1,36 @@
+--select * from forum_posts
+
+--select *
+-- from forum_posts
+-- where title like '%EmptyStack%'
+-- or content like '%EmptyStack%';
+
+-- "smart-money-44"
+
+-- select *
+-- from forum_accounts
+-- where username like '%smart-money-44%';
+
+--Brad Steele
+
+-- select *
+-- from forum_accounts
+-- where last_name like '%Steele%';
+
+--Andrew Steele creds
+
+-- select *
+-- from emptystack_messages
+-- where subject like '%TAXI%';
+
+
+--your-boss-99
+-- password is notagaincarter
+
+-- name is skylar singer
+
+-- "TAXI" project code
+
+-- DczE0v2b project id
+
+
